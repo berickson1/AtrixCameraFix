63a51836fb48ae5d5c5025615ad4443a5a0ca69a
Brent Erickson <brent2737@gmail.com>
2013-01-01 16:52:13 -0700

    Add ability to kill the Gallery app when running the fix. Use killall to kill processes instead of kill/pidof

--- Atrix Camera Fix/src/ca/berickson/atrix/camfix/CameraReset.java
+++ Atrix Camera Fix/src/ca/berickson/atrix/camfix/CameraReset.java
@@ -1,72 +1,118 @@
 package ca.berickson.atrix.camfix;
 
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
-import android.R.string;
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Context;
-import android.view.Menu;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewDebug.IntToString;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.Toast;
 
 public class CameraReset extends Activity {
 
-	private static Button btnKillMediaServer;
+    private static final String LOG_TAG = "Atrix.CamFix";
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_camera_reset);
-		btnKillMediaServer = (Button) this.findViewById(R.id.btnKillMediaServer);
-		btnKillMediaServer.setOnClickListener(new View.OnClickListener() {
+    // Controls
+    private static Button btnKillMediaServer;
+    private static CheckBox chkKillGallery;
 
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				killMediaServer();
-			}
-		});
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_reset);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_camera_reset, menu);
-		return true;
-	}
+        // Find Controls
+        btnKillMediaServer = (Button) this
+                .findViewById(R.id.btnKillMediaServer);
+        chkKillGallery = (CheckBox) this.findViewById(R.id.chkKillGallery);
 
-	private void killMediaServer() {
-		try {
-			Process process;
-			process = Runtime.getRuntime().exec("su");
-			DataOutputStream os = new DataOutputStream(process.getOutputStream());
-			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			os.writeBytes("ls -l\n");
-			// Waiting here for root
-			reader.readLine();
-			reader.close();
-			os.writeBytes("kill -9 `pidof mediaserver`\n");
-			os.writeBytes("exit\n");
-			os.flush();
-			os.close();
-			process.waitFor();
-			displayToast("Done!");
-		} catch (IOException e) {
-			displayToast("An Error Occurred. Did you grant root privileges?");
-		} catch (Exception e) {
-			displayToast("OOPS! Something bad happened! Please report this.");
-			e.printStackTrace();
-		}
-	}
+        // Setup Button on-click
+        btnKillMediaServer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                killGallery();
+                killMediaServer();
+                displayToast("Done!");
+            }
+        });
 
-	private void displayToast(String msg) {
-		Context context = getApplicationContext();
-		Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+        // Set CheckBox State
+        chkKillGallery.setChecked(getGalleryCheck());
 
-	}
+        // Setup CheckBox on-check
+        chkKillGallery
+                .setOnCheckedChangeListener(new OnCheckedChangeListener() {
+                    public void onCheckedChanged(CompoundButton buttonView,
+                            boolean isChecked) {
+                        setGalleryCheck(isChecked);
+                    }
+                });
+    }
+
+    private void killMediaServer() {
+        killProcess("mediaserver");
+    }
+
+    private void killGallery() {
+        if (chkKillGallery.isChecked()) {
+            Log.d(LOG_TAG, "Gallery CheckBox set");
+            killProcess("com.android.gallery3d");
+        }
+    }
+
+    private void setGalleryCheck(Boolean value) {
+        SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+        editor.putBoolean("kill-gallery", value);
+        editor.commit();
+    }
+
+    private Boolean getGalleryCheck() {
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        return prefs.getBoolean("kill-gallery", true);
+    }
+
+    /* Utility Functions */
+    private void killProcess(String proc) {
+        executeAsRoot("killall " + proc + "");
+    }
+
+    private void displayToast(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    }
+
+    private void executeAsRoot(String command) {
+        try {
+            Process process;
+            process = Runtime.getRuntime().exec("su");
+            DataOutputStream os = new DataOutputStream(
+                    process.getOutputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    process.getInputStream()));
+            os.writeBytes("ls -l\n");
+            // Waiting here for root
+            reader.readLine();
+            reader.close();
+            os.writeBytes(command + "\n");
+            os.writeBytes("exit\n");
+            os.flush();
+            os.close();
+            process.waitFor();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Caught an IO exception.", e);
+            displayToast("An Error Occurred. Did you grant root privileges?");
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Caught an exception.", e);
+            displayToast("OOPS! Something bad happened! Please report this.");
+        }
+    }
 }
